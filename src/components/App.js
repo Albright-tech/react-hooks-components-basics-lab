a7af61b78c5026b557b0c62e025d9b80b514c3d7
Albright <albright.muchori@student.moringaschool.com>
2023-10-27 10:51:29 +0300

    components basic lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About Us</h2>
+      <p>This is the About Us section of our website.</p>
+    </div>
+  );
+}
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
